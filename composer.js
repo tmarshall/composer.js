2f34bb81fe4f411b62bbfc625d3ae6fe6a7d1f50
Andrew Danger Lyon <orthecreedence@gmail.com>
2011-11-03 12:40:53 -0700

    fixing bug in Model.defaults

--- composer.js
+++ composer.js
@@ -216,8 +216,11 @@
 		 */
 		initialize: function(data, options)
 		{
-			// merge the defaults into the data
-			data	=	Object.merge(this.defaults, data);
+			data || (data = {});
+
+			// merge the defaults into the data (note the Object.clone(...) around 
+			// defaults, which fixes a bug that was popping up). 
+			data	=	Object.merge(Object.clone(this.defaults), data);
 
 			// set the data into the model (but don't trigger any events)
 			this.set(data, {silent: true});
